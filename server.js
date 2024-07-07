f5762e88ad9575dfa595ae36c31fedf5622250d5
Debjitdolui <debjitdolui04@gmail.com>
2024-07-07 09:25:26 +0530

    change in mgdb atlas

--- server.js
+++ server.js
@@ -40,13 +40,9 @@ app.use('/api/payment',paymentRouter)
 
 mongoose
   .connect(
-    "mongodb+srv://user01:User01@cluster0.jlrd74y.mongodb.net/",
+    "mongodb+srv://debjitdolui04:CdXbKyru0soT03ZZ@cluster0.jlrd74y.mongodb.net/",
     {
       dbName: "MERN_E_Commerce",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 10000, // 10 seconds
-      socketTimeoutMS: 45000, // 45 seconds
     }
   )
   .then(() => console.log("MongoDB Connected Successfully...!"))
